427dc59481a788ee772239b5d695a65708c6f425
Vova Yankers <vovarojko@gmail.com>
2024-07-10 22:48:17 +0300

    fixHeader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import AboutUs from '../components/AboutUs';
 import Carousel from '../components/Carousel';
 import AppCart from '../components/Cart';
 
-import Header from '../components/Header';
 import NavigationBar from '../components/NewHeader';
 import ProductsList from '../components/ProductsList';
 // import ProductsList from '../components/ProductsList';
