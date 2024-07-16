3ba00aa3a6b335f2681439ce4de7f1840a803773
Vova Yankers <vovarojko@gmail.com>
2024-07-16 10:36:30 +0300

    Revert "buttonReg"

--- src/components/Form/SignIn/index.tsx
+++ src/components/Form/SignIn/index.tsx
@@ -1,19 +1,9 @@
-import { Link } from 'react-router-dom';
-import buttonReg from '../../../assets/buttons/butttonReg.svg';
-import exitIcon from '../../../assets/icons/tabler-icon-x.svg';
-
 import Button from '../../Button';
 import styles from './SignInForm.module.scss';
-
 const SignIn = () => {
   return (
     <div className={styles.formContainer}>
       <div className={styles.formWrapper}>
-        <div className={styles.formExit}>
-          <Link to="/">
-            <img src={exitIcon}></img>
-          </Link>
-        </div>
         <h2 className={styles.formTitle}>Вхід</h2>
         <form>
           <div className={styles.formGroup}>
@@ -34,16 +24,9 @@ const SignIn = () => {
           <div className={styles.formGroup}>
             <input placeholder="Пароль" className={styles.formInput}></input>
           </div>
-          <div className={styles.formText}>Або</div>
+          <div className={styles.formGroup}>Або</div>
           <div className={styles.formGroup}>
-            <div className={styles.buttonRegContainer}>
-              <Button className={styles.buttonRegisterPhone}>
-                Увійти по номеру телефону
-              </Button>
-              <Button>
-                <img src={buttonReg}></img>
-              </Button>
-            </div>
+            <Button>Зареєструватися</Button>
           </div>
         </form>
       </div>
