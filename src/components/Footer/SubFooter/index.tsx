02980c916f2d78d9a05c071633a8ffd217e22c07
Vova Yankers <vovarojko@gmail.com>
2024-07-28 19:48:25 +0300

    Footer new

--- src/components/Footer/SubFooter/index.tsx
+++ src/components/Footer/SubFooter/index.tsx
@@ -1,23 +1,27 @@
 import styles from './SubFooter.module.scss';
+import delivery from '../../../assets/iImageSubFooter/delivery.svg';
+import consultation from '../../../assets/iImageSubFooter/consultation.svg';
+import garanty from '../../../assets/iImageSubFooter/garanty.svg';
+import repair from '../../../assets/iImageSubFooter/repair.svg';
 
 const SubFooter = () => {
   return (
     <div className={styles.WrapperFooter}>
       <div className={styles.footer}>
         <div className={styles.footerItem}>
-          <img src="/path-to-your-icon-1.svg" alt="Delivery Icon" />
+          <img src={delivery} alt="Delivery Icon" />
           <p>Безкоштовна та швидка доставка</p>
         </div>
         <div className={styles.footerItem}>
-          <img src="/path-to-your-icon-2.svg" alt="Warranty Icon" />
+          <img src={garanty} alt="Warranty Icon" />
           <p>Гарантований обмін та повернення</p>
         </div>
         <div className={styles.footerItem}>
-          <img src="/path-to-your-icon-3.svg" alt="Consultation Icon" />
+          <img src={consultation} alt="Consultation Icon" />
           <p>Швидкі та якісні консультації</p>
         </div>
         <div className={styles.footerItem}>
-          <img src="/path-to-your-icon-4.svg" alt="Setup Icon" />
+          <img src={repair} alt="Setup Icon" />
           <p>Можливість замовити налаштування та монтаж</p>
         </div>
       </div>
