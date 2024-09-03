617d2ee12584a262b1bb7ed328ace0baa932d50e
Vova Yankers <vovarojko@gmail.com>
2024-09-03 14:37:20 +0300

    register simple sign-in new

--- src/components/NewHeader/NavigationBar.tsx
+++ src/components/NewHeader/NavigationBar.tsx
@@ -1,5 +1,5 @@
 // src/components/NavigationBar.tsx
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './NavigationBar.module.scss';
 const NavigationBar: React.FC = () => {
