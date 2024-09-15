7c271909b1a0564d52ea5443709f700e2794831d
andrpavl <117930401+andrpavl@users.noreply.github.com>
2024-09-15 13:21:54 +0200

    !

--- src/redux/cart/cartSlice.ts
+++ src/redux/cart/cartSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { addCartItem, deleteCartItem, fetchCart } from './operations';
 
-interface CartItem {
+export interface CartItem {
   id: number;
   name: string;
   price: number;
@@ -8,43 +9,77 @@ interface CartItem {
   quantity: number;
 }
 
-
 interface CartState {
   totalPrice: number;
   cartItems: CartItem[];
+  error: any;
 }
 
 const initialState: CartState = {
   totalPrice: 0,
   cartItems: [],
+  error: null,
 };
+
+// const cartSlice = createSlice({
+//   name: 'cart',
+//   initialState,
+//   reducers: {
+//     addItem(state, action: PayloadAction<CartItem>) {
+//       const existingItem = state.cartItems.find(
+//         item => item.id === action.payload.id
+//       );
+//       if (existingItem) {
+//         existingItem.quantity += action.payload.quantity;
+//       } else {
+//         state.cartItems.push(action.payload);
+//       }
+//       state.totalPrice += action.payload.price * action.payload.quantity;
+//     },
+//     deleteItem(state, action: PayloadAction<number>) {
+//       const itemToDelete = state.cartItems.find(
+//         item => item.id === action.payload
+//       );
+//       if (itemToDelete) {
+//         state.totalPrice -= itemToDelete.price * itemToDelete.quantity;
+//         state.cartItems = state.cartItems.filter(
+//           item => item.id !== action.payload
+//         );
+//       }
+//     },
+//     increaseQuantity(state, action: PayloadAction<number>) {
+//       const item = state.cartItems.find(item => item.id === action.payload);
+//       if (item) {
+//         item.quantity += 1;
+//         state.totalPrice += item.price;
+//       }
+//     },
+//     decreaseQuantity(state, action: PayloadAction<number>) {
+//       const item = state.cartItems.find(item => item.id === action.payload);
+//       if (item && item.quantity > 1) {
+//         item.quantity -= 1;
+//         state.totalPrice -= item.price;
+//       } else if (item?.quantity === 1) {
+//         state.totalPrice -= item.price;
+//         state.cartItems = state.cartItems.filter(
+//           cartItem => cartItem.id !== item.id
+//         );
+//       }
+//     },
+//    },
+// });
+
+// export const {
+//   addItem,
+//   deleteItem,
+//   increaseQuantity,
+//   decreaseQuantity,
+// } = cartSlice.actions;
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem(state, action: PayloadAction<CartItem>) {
-      const existingItem = state.cartItems.find(
-        item => item.id === action.payload.id
-      );
-      if (existingItem) {
-        existingItem.quantity += action.payload.quantity;
-      } else {
-        state.cartItems.push(action.payload);
-      }
-      state.totalPrice += action.payload.price * action.payload.quantity;
-    },
-    deleteItem(state, action: PayloadAction<number>) {
-      const itemToDelete = state.cartItems.find(
-        item => item.id === action.payload
-      );
-      if (itemToDelete) {
-        state.totalPrice -= itemToDelete.price * itemToDelete.quantity;
-        state.cartItems = state.cartItems.filter(
-          item => item.id !== action.payload
-        );
-      }
-    },
     increaseQuantity(state, action: PayloadAction<number>) {
       const item = state.cartItems.find(item => item.id === action.payload);
       if (item) {
@@ -64,16 +99,49 @@ const cartSlice = createSlice({
         );
       }
     },
-   },
+  },
+  extraReducers: builder =>
+    builder
+      .addCase(fetchCart.fulfilled, (state, action) => {
+        state.cartItems = action.payload.items;
+        state.totalPrice = state.cartItems.reduce(
+          (total, item) => total + item.price * item.quantity,
+          0
+        );
+      })
+      .addCase(fetchCart.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(addCartItem.fulfilled, (state, action) => {
+        const existingItem = state.cartItems.find(
+          item => item.id === action.payload.id
+        );
+        existingItem
+          ? (existingItem.quantity += action.payload.quantity)
+          : state.cartItems.push(action.payload);
+
+        state.totalPrice += action.payload.price * action.payload.quantity;
+      })
+      .addCase(addCartItem.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(deleteCartItem.fulfilled, (state, action) => {
+        const itemToDelete = state.cartItems.find(
+          item => item.id === action.payload
+        );
+        if (itemToDelete) {
+          state.totalPrice -= itemToDelete.price * itemToDelete.quantity;
+          state.cartItems = state.cartItems.filter(
+            item => item.id !== action.payload
+          );
+        }
+      })
+      .addCase(deleteCartItem.rejected, (state, action) => {
+        state.error = action.error.message;
+      }),
 });
-
-export const {
-  addItem,
-  deleteItem,
-  increaseQuantity,
-  decreaseQuantity,
-} = cartSlice.actions;
-
+export const { increaseQuantity, decreaseQuantity } =
+  cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
 
 export default cartReducer;
---
+++ src/redux/cart/operations.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const shoppingCartUrl = 'https://dev.smarthome-team.store/api/ShoppingCart/';
+
+export const fetchCart = createAsyncThunk(
+  'cart/fetchCartItems',
+  async (cartId: any, thunkAPI) => {
+    try {
+      const response = await axios.get(`${shoppingCartUrl}${cartId}`, cartId);
+      return response.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addCartItem = createAsyncThunk(
+  'cart/addCartItem',
+  async (cartData: { id: string; items: Array<{ productId: number; productName: string; price: number; quantity: number; pictureUrl: string; }>; deliveryMethodId: number; clientSecret: string; paymentIntentId: string }, thunkAPI) => {
+    try {
+      const response = await axios.post(shoppingCartUrl, cartData);
+      return response.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+
+export const deleteCartItem = createAsyncThunk(
+  'cart/deleteCartItem',
+  async (itemId: number, thunkAPI) => {
+    try {
+      await axios.delete(`${shoppingCartUrl}${itemId}`);
+      return itemId;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
